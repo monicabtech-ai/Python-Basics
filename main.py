8e98bff018ef3d36790e663bbd391f406bfa2e1b
monicabtech-ai <monica.b.tech@gmail.com>
2025-11-30 21:19:44 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# Printin a Greeting Message
+print("Hello, World!")
+
+# Printing my name
+print("My name is Alex.")
+
+# Printing a fun fact about myself
+print("I love playing soccer.")
+
+# Performing and printing a simple calculation
+print("The result of 3 + 5 is", 3 + 5)
